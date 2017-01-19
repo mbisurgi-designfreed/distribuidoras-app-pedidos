c56f9646d300448ef5df30e08fb91357493f95fe
mbisurgi <maximiliano@designfreed.com>
2017-01-19 13:15:39 -0300

    Initial commit

---
+++ app/src/main/java/com/designfreed/distribuidoras_app_pedidos/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.designfreed.distribuidoras_app_pedidos.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.designfreed.distribuidoras_app_pedidos.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
