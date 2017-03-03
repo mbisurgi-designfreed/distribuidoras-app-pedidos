a62e09fba6e27d00e8cfe7df19f351822db2b06f
mbisurgi <maximiliano@designfreed.com>
2017-03-03 13:10:52 -0300

    Constante para facil cambio de empresa

--- app/src/main/java/com/designfreed/distribuidoras_app_pedidos/constants/Constants.java
+++ app/src/main/java/com/designfreed/distribuidoras_app_pedidos/constants/Constants.java
@@ -1,5 +1,5 @@
 package com.designfreed.distribuidoras_app_pedidos.constants;
 
 public class Constants {
-    public static final String SERVER = "http://186.153.8.242:8080/";
+    public static final String SERVER = "http://bybgas.dyndns.org:8080/";
 }
